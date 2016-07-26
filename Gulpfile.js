388e23a58b274b5fce024097b31d8239622b6fd9
ilvy <478283225@qq.com>
2016-07-26 09:59:54 +0800

    gulp build

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,28 +5,30 @@ var plugins = gulpLoadPlugins();
 var del = require('del');
 var runSequence = require('run-sequence');
 var srcPath = 'public/',
-	distPath = 'public-dist/';
-
+	distPath = 'public-dist/',
+	tmpPath = 'public-tmp/',
+	viewsPath = 'views/',
+	viewsDistPath = 'views-dist/';
+var distAssetsPath = distPath + 'stylesheets/';
+var srcAssetsPath = srcPath + 'stylesheets/';
+var tmpAssetsPath = tmpPath + 'stylesheets/';
 
 /******************* 正式环境 ********************/
 gulp.task('clean',function(){
-	return del([distPath]);
+	return del([distPath,tmpPath,viewsDistPath,"rev/"]);
 });
 
 gulp.task('uglifyjs',function(){
 	return gulp.src([srcPath+"javascripts/**/*.js",srcPath+"Jplugin/**/*.js",srcPath+"modules/**/*.js",srcPath+"modules/**/*.js",srcPath+"util/**/*.js",srcPath+"*.js"],{base:srcPath})
 		       .pipe(plugins.uglify())
-		       .pipe(gulp.dest(distPath));
+		       .pipe(gulp.dest(tmpPath));
 });
 
-var distAssetsPath = distPath + 'stylesheets/';
-var srcAssetsPath = srcPath + 'stylesheets/';
 
 gulp.task('concatcss',function(){
-	var stylesPath = srcPath + 'stylesheets/';
 	var concatCssFiles = [srcPath+'Jplugin/jquery.light-popup/jquery.light-popup.css'];
 	['bootstrap.css','bootstrap-grid.css','mystyle.css','style_new.css'].forEach(function(item,i){
-		concatCssFiles.push(stylesPath + item);
+		concatCssFiles.push(srcAssetsPath + item);
 	});
 	return gulp.src(concatCssFiles,{base:srcPath})
 			   .pipe(plugins.concat('all.css'))
@@ -34,35 +36,69 @@ gulp.task('concatcss',function(){
 });
 
 gulp.task('minifycss',function(){
-	return gulp.src([srcAssetsPath+'all.css'],{base:srcAssetsPath})
+	return gulp.src([srcAssetsPath+'*.css'],{base:srcAssetsPath})
 			   .pipe(plugins.minifyCss())
-			   .pipe(gulp.dest(distAssetsPath));
+			   .pipe(gulp.dest(tmpAssetsPath));
 
 });
 
 gulp.task('md5-cssjs',function(){
-	return gulp.src([distPath+"**/*.js",distAssetsPath+"**/*.css","!"+distPath+"jqr.js"],{base:distPath})
+	return gulp.src([tmpPath+"**/*.js",tmpPath+"**/*.css","!"+tmpPath+"jqr.js","!"+tmpPath+"Jplugin/laydate/**/*"],{base:tmpPath})
 			   .pipe(plugins.rev())
 			   .pipe(gulp.dest(distPath))
-			   .pipe(plugins.rev.manifest())
-			   .pipe(gulp.dest(distPath));
+			   .pipe(plugins.rev.manifest('rev/rev-manifest.json',{merge:true,base: process.cwd()+'/rev'}))
+			   .pipe(gulp.dest("rev"));
 });
 
 gulp.task('dealrevjson',function () {
-	fs.readFile(distPath+'rev-manifest.json','utf-8',function(err,data){
-		data = data.replace(/\.js/g,'');
-		fs.writeFile(distPath+'rev-manifest.json',data,function(err){
+	var data = fs.readFileSync('rev/rev-manifest.json').toString('utf8').replace(/\.js/g,'');
+	fs.writeFileSync('rev/rev-manifest.json',data);
+	// fs.readFile(distPath+'rev/rev-manifest.json','utf-8',function(err,data){
+	// 	data = data.replace(/\.js/g,'');
+	// 	fs.writeFile(distPath+'rev/rev-manifest.json',data,function(err){
 
-		});
-	});
+	// 	});
+	// });
 });
 
 gulp.task('revReplace',function(){
-	var manifest = gulp.src(distPath+"rev-manifest.json");
-	return gulp.src([distPath+"jqr.js"],{base:distPath})
+	var manifest = gulp.src("rev/rev-manifest.json");
+	// console.log(manifest)
+	return gulp.src([tmpPath+"jqr.js"],{base:tmpPath})
 			   .pipe(plugins["revReplace"]({manifest:manifest}))
 			   .pipe(plugins.rev())
+			   .pipe(gulp.dest(distPath))
+			   .pipe(plugins.rev.manifest('rev/rev-manifest.json',{merge:true,base: process.cwd()+'/rev'}))
+			   .pipe(gulp.dest("rev"));
+});
+
+gulp.task('revReplaceHtml',function(){
+	var manifest = gulp.src("rev/rev-manifest.json");
+	// console.log(manifest)
+	return gulp.src([srcPath+"*.html"],{base:srcPath})
+			   .pipe(plugins["revReplace"]({manifest:manifest}))
 			   .pipe(gulp.dest(distPath));
+});
+
+
+gulp.task('revReplaceJade',function(){
+	var manifestJson = require('./rev/rev-manifest.json');
+	return gulp.src([viewsPath+"*.jade"],{base:viewsPath})
+			   .pipe(through2.obj(function(file,enc,cb){
+			   	// console.log(file.contents.toString());
+			   	var fileContents = file.contents.toString();
+			   	for(var key in manifestJson){
+			   		var regStr = new RegExp(key,"g");
+					fileContents = fileContents.replace(regStr,manifestJson[key]);
+				}
+			   	// console.log((fileContents.match(reg))[1])
+			   	// var jsFileSrc = srcPath + fileContents.match(reg)[1];
+			   	
+			   	// console.log(resultFile)
+			   	file.contents = new Buffer(fileContents,"utf8");
+			   	cb(null,file);
+			   }))
+			   .pipe(gulp.dest(viewsDistPath));
 });
 
 var through2 = require('through2');
@@ -70,7 +106,7 @@ var through2 = require('through2');
  * 静态页面部分js文件内联写入
  */
 gulp.task('inlinejs',function(){
-	return gulp.src([srcPath+'*.html'],{base:srcPath})
+	return gulp.src([distPath+'*.html'],{base:distPath})
 			   .pipe(through2.obj(function(file,enc,cb){
 			   	// console.log(file.contents.toString());
 			   	var fileContents = file.contents.toString();
@@ -78,17 +114,16 @@ gulp.task('inlinejs',function(){
 			   	// console.log((fileContents.match(reg))[1])
 			   	// var jsFileSrc = srcPath + fileContents.match(reg)[1];
 			   	var resultFile = fileContents.replace(reg,function(match, src, index, input) {
-			   		console.log("************",src);
                     return ['<script>',
-                        fs.readFileSync(srcPath+src).toString('utf8'),
+                        fs.readFileSync(distPath+src).toString('utf8'),
                         '<\/script>'
                     ].join("");
                 });
-			   	console.log(resultFile)
+			   	// console.log(resultFile)
 			   	file.contents = new Buffer(resultFile,"utf8");
 			   	cb(null,file);
 			   }))
-			   .pipe(plugins.htmlmin({collapseWhitespace: true}))
+			   // .pipe(plugins.htmlmin({collapseWhitespace: true}))
 			   .pipe(gulp.dest(distPath));
 });
 
@@ -97,7 +132,7 @@ gulp.task('inlinejs',function(){
 
 
 gulp.task('build',['clean'],function(){
-	runSequence('uglifyjs','concatcss','minifycss','md5-cssjs','dealrevjson','revReplace');
+	runSequence('uglifyjs','concatcss','minifycss','md5-cssjs','dealrevjson','revReplace','revReplaceHtml','inlinejs','revReplaceJade');
 });
 
 gulp.task('watch',function(){
